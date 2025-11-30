cd4ea6e8ddd6198c681a303b6a558ce2394cc44b
tusin777 <ruslantusin@gmail.com>
2025-11-30 22:18:20 +0100

    Удалить файлы App.jsx и App.css, обновить структуру импорта и исправить стили в компоненте App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,34 @@
-import "./App.css";
-import { WelcomeMessage } from "../components/WelcomeMessage/WelcomeMessage.jsx";
+import { UserGreeting } from "../UserGreeting/UserGreeting.jsx";
+import UserGreetingDefault from "../UserGreeting/UserGreetingDefault.jsx";
+// именованный экспорт
+import { UserGreeting3 } from "../UserGreeting/UserGreetingMixed.jsx";
 
-function App() {
+// экспорт по умолчанию
+import UserGreeting3Default from "../UserGreeting/UserGreetingMixed.jsx";
+
+import { ElementExample } from "../ElementExample/ElementExample.jsx";
+import ProductTitle from "../ProductTitle/ProductTitle.jsx";
+import { SiteInfo, UserInfo } from "../InfoBlock/InfoBlock.jsx";
+
+import "./App.css";
+
+export default function App() {
   return (
     <div className="app-container">
-      <h1>React Homework Template</h1>
-      <p>Здесь будет домашнее задание к соответствующему уроку.</p>
-      <WelcomeMessage />
+      <h1>React Lesson Template</h1>
+      <p>Этот шаблон предназначен для использования в любом уроке.</p>
+      <UserGreeting />
+      <UserGreeting />
+      <UserGreeting />
+      <UserGreetingDefault />
+      <UserGreeting3 />
+      <UserGreeting3Default />
+      <ElementExample />
+      <ProductTitle />
+      <ProductTitle />
+      <ProductTitle />
+      <SiteInfo />
+      <UserInfo />
     </div>
   );
 }
-
-export default App;
