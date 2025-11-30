41069f72c8cbefde27bcdf071d1ae69fde45336a
tusin777 <ruslantusin@gmail.com>
2025-11-30 21:43:50 +0100

    Добавить начальную настройку проекта с использованием React и Vite, включая конфигурацию ESLint, стили и основные компоненты.

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>React Homework Template</h1>
+      <p>Здесь будет домашнее задание к соответствующему уроку.</p>
+    </div>
+  );
+}
+
+export default App;
